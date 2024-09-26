6d09b1106fcc1f3d752f51b38a76582d554e5532
Emile0001 <boteseh@gmail.com>
2024-09-26 05:58:00 +0200

    UseState Array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+    // set data as default value
+    const [people, setPeople] = useState(data);
+
+    const removeItem = (id) => {
+        setPeople(people.filter((person) => person.id !== id));
+    };
+
+    const clearAll = () => {
+        setPeople([]);
+    };
+    return (
+        <>
+            {people.map((person) => {
+                const { id, name } = person;
+
+                return (
+                    <ul key={id}>
+                        <li>
+                            {name}
+                            {"   "}
+                            <button
+                                type="button"
+                                className="btn"
+                                onClick={() => {
+                                    removeItem(id);
+                                }}
+                            >
+                                Remove
+                            </button>
+                        </li>
+                    </ul>
+                );
+            })}
+            <button type="button" className="btn" onClick={clearAll}>
+                Clear List
+            </button>
+        </>
+    );
 };
 
 export default UseStateArray;
