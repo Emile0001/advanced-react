4b5ea3fe8978648ffbeba102184b21157db38ac1
Emile0001 <boteseh@gmail.com>
2024-09-26 11:32:26 +0200

    useState Gotcha

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+    const [count, setCount] = useState(0);
+    const increaseCount = () => {
+        setCount(count + 1);
+    };
+    return (
+        <>
+            <h2>{count}</h2>
+            <button className="btn" type="button" onClick={increaseCount}>
+                Increase Count
+            </button>
+        </>
+    );
 };
 
 export default UseStateGotcha;
