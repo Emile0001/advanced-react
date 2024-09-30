37a2e0d7c72793db901bd625b4bc133ae96b8a6c
Emile0001 <boteseh@gmail.com>
2024-09-30 04:09:47 +0200

    function invoke

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
 
-  return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
-  );
+    function triggerFunction() {
+        alert("I will be triggered with every re-render");
+    }
+    // will be invoked with every re-render
+    triggerFunction();
+
+    return (
+        <div>
+            <h1>value : {value}</h1>
+            <button className="btn" onClick={() => setValue(value + 1)}>
+                click me
+            </button>
+        </div>
+    );
 };
 export default CodeExample;
